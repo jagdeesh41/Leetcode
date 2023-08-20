ba83eb72e6370b895bdaf6429137bf2b2717033b
jagdeesh41 <71251158+jagdeesh41@users.noreply.github.com>
2023-08-20 20:34:08 +0530

    Update Binary Tree Paths.py

--- Binary Tree Paths.py
+++ Binary Tree Paths.py
@@ -1,3 +1,6 @@
+#257. Binary Tree Paths
+#https://leetcode.com/problems/binary-tree-paths/
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
